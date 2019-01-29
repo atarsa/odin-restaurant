f6f9fe522099b166e35d6ee4073be21e46b71726
ATarsa <smb909@gmail.com>
2019-01-29 13:50:00 +0000

    Add navigation tabs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import { pageLoad } from './pageload'
+import { pageLoad } from './js/pageload'
+import { aboutTab } from './js/aboutTab'
+import { menuTab } from './js/menuTab'
+import { contactTab} from './js/contactTab'
 import style from "./_scss/main.scss";
 
-const content = document.getElementById('content');
 
+// append homepage view
+const content = document.getElementById('content');
 content.appendChild(pageLoad());
+
+const mainDiv = document.querySelector('.main');
+mainDiv.appendChild(aboutTab());
+
+// add Event Listener to navigation
+const nav = document.querySelector('nav');
+nav.addEventListener('click', function(e){
+  // clear previous 
+  mainDiv.innerHTML = '';
+  console.log(e.target.innerText)
+  if(e.target.innerText == 'About'){
+      mainDiv.appendChild(aboutTab());
+      
+  } 
+  else if (e.target.innerText == 'Menu'){
+      mainDiv.appendChild(menuTab());
+      
+  } 
+  else if (e.target.innerText == 'Contact'){
+      mainDiv.appendChild(contactTab());
+      
+  }
+  e.target.classList.toggle('active');
+})
--- src/js/pageload.js
+++ src/js/pageload.js
@@ -15,33 +15,56 @@ function pageLoad(){
   const p = document.createElement('p');
   p.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipicing elit pellentesque. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Eget dolor morbi non arcu risus quis. Pretium quam vulputate dignissim suspendisse in est";
 
+  const contactDiv = document.createElement('div');
+  contactDiv.classList.add('contact-info');
+  contactDiv.innerHTML = `<hr><p> 3502 Keyser Ridge Road    <br>
+      Greensboro, NC 27406
+      info@mysite.com <br>
+      123 456 6780 <br>
+      Open everyday 2pm to 12am
+      </p>
+      <small>
+   Created with Vanilla JS by <a href="https://github.com/atarsa"> A.Tarsa </a>
+  </small>`;
+
   sideDiv.appendChild(h1);
   sideDiv.appendChild(hr);
   sideDiv.appendChild(h3);
   sideDiv.appendChild(p);
+ 
+  sideDiv.appendChild(contactDiv);
 
   // create navigation menu
   const navUI = document.createElement('nav');
   const ul = document.createElement('ul');
+  
+  const liAbout = document.createElement('li');
+  liAbout.innerText = 'About'
+ 
   const liMenu = document.createElement('li');
   liMenu.innerText = 'Menu';
-  const liLocation = document.createElement('li');
-  liLocation.innerText = 'Location';
+  
   const liContact = document.createElement('li');
   liContact.innerText = 'Contact';
 
+  ul.appendChild(liAbout);
   ul.appendChild(liMenu);
-  ul.appendChild(liLocation);
   ul.appendChild(liContact);
+
+  // create div for main content
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('main');
 
   navUI.appendChild(ul);
 
   
   containerDiv.appendChild(sideDiv);
   containerDiv.appendChild(navUI);
+  containerDiv.appendChild(mainDiv);
 
   return containerDiv;
   
 }
+
 
 export { pageLoad };
